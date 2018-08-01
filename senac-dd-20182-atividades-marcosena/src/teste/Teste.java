8dae58e69f6e7bc5a11bfcf126ba2c3229ce4482
marcoacsena <marcoacsenagmail.com>
2018-08-01 20:54:28 -0300

    primeiro commit.

---
+++ senac-dd-20182-atividades-marcosena/src/teste/Teste.java
@@ -0,0 +1,12 @@
+package teste;
+
+import javax.swing.JOptionPane;
+
+public class Teste {
+
+	public static void main(String[] args) {
+		JOptionPane.showConfirmDialog(null, "Confirmar");
+
+	}
+
+}
